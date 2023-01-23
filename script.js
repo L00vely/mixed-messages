c288d57e712a2ccdba12d7cfecc66f9cbba263e4
L00vely <peralesrodriguezdavid@gmail.com>
2023-01-22 20:36:41 -0600

    Finalizando el código

--- script.js
+++ script.js
@@ -1,42 +1,25 @@
 function generateRandomNumber(num) {
-    // Gets # from 0 -> num - 1
-    return Math.floor(Math.random() * num)
-  }
+  // Gets # from 0 -> num - 1
+  return Math.floor(Math.random() * num)
+}
+
+
+console.log(generateRandomNumber(10))
   
-  const collectiveWisdom = {
-    signInfo: ['star', 'moon', 'sun', 'comet'],
-    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
-    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
-  }
+const randomStuff = {
+  movies: ['Zoolander', 'Bullet Train', 'Shrek', 'Knives Out', 'Kick Ass', 'Puss In Boots 2: The last wish', 'M3GAN', 'El Camino', 'Suicide Squad'],
+  videogame: ['The Last of Us', 'Project Zomboid', 'Fallout New Vegas', 'Overwatch 2', 'Team Fortress 2', 'Minecraft', 'Fornite', 'GTA V', 'Red Dead Redemption 2'],
+  musician: ['Magdalena Bay', 'Daft Punk', 'Justice', 'LMFAO', 'José José', 'Bad Bunny', 'Dualipa', 'The Weeknd']
+}
   
-  // Store the 'wisdom' in an array
-  let personalWisdom = []
-  
-  // Iterate over the objects
-  for(let prop in collectiveWisdom) {
-    let optionIdx = generateRandomNumber(collectiveWisdom[prop].length)
-  
-    // use the object's properties to customize the message being added to personalWisdom  
-    switch(prop) {
-      case 'signInfo':
-        personalWisdom.push(`Your sign right now is a "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'fortuneOutput':
-        personalWisdom.push(`You are having: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'advice':
-        personalWisdom.push(`You should: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      default:
-        personalWisdom.push('There is not enough info.')
-    }
-  }
-  
-  function formatWisdom(wisdom) {
-    // Add some ASCII here?
-    // Add in more symbols to the array? like emojies or what not?
-    const formatted = personalWisdom.join('\n')
-    console.log(formatted)
-  }
-  
-  formatWisdom(personalWisdom);
+
+const returnElement = (arr) =>{
+  const index = generateRandomNumber(arr.length);
+  return arr[index]
+}
+
+
+
+console.log(`Random movie: ${returnElement(randomStuff.movies)}`);
+console.log(`Random videogame: ${returnElement(randomStuff.videogame)}`);
+console.log(`Random musician: ${returnElement(randomStuff.musician)}`);
